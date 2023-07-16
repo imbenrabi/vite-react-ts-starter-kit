762ca9658c978aaddccda7717c2206938c73a513
Ben Rabi <benr@wix.com>
2023-07-16 13:59:50 +0300

    add(): init cleanup

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-type Foo = {
-    bar: string;
-}
-
 const App = () => {
-    const foo: Foo = {
-        bar: '1'
-    };
-
-  return <div>Hello, Vite + React</div>;
+    return <div>Hello, Vite + React</div>;
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
